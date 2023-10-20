03ceb030d480f2694de3a6e21518719fa8d7d9c8
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-21 06:09:56 +0700

    implement Task migration

---
+++ database/migrations/2023_10_20_230658_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id("task_id");
+            $table->unsignedBigInteger("user_id");
+            $table->string("task_name",100);
+            $table->text("description");
+            $table->date("started");
+            $table->date("deadline");
+            $table->enum("status", ["1", "2", "3"]);
+            $table->foreign("user_id")->references("user_id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
