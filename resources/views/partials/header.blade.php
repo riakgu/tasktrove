92a95463df467c22bf6783d87e563bd0f7d86b07
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-21 15:05:16 +0700

    add header to partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,54 @@
+<header>
+    <nav class="navbar navbar-expand navbar-light navbar-top">
+        <div class="container-fluid">
+            <a href="#" class="burger-btn d-block">
+                <i class="bi bi-justify fs-3"></i>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-lg-0">
+
+                </ul>
+                <div class="dropdown">
+                    <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="user-menu d-flex">
+                            <div class="user-name d-flex align-items-center me-2">
+                                <div class="user-name text-end me-3">
+                                    <h6 class="mb-0 text-gray-600">Rizky</h6>
+                                    <p class="mb-0 text-sm text-gray-600">User</p>
+                                </div>                                        </div>
+                            <div class="user-img d-flex align-items-center">
+                                <div class="avatar avatar-md">
+                                    <img src="assets/images/jpg/1.jpg" />
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton"
+                        style="min-width: 11rem">
+                        <li>
+                            <a class="dropdown-item" href="settings.html"><i class="icon-mid bi bi-gear me-2"></i>
+                                Settings</a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li>
+                            <form action="#" method="post">
+
+                                <button type="submit" class="dropdown-item">
+                                    <i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                                    Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
