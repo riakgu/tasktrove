2b17686499a1da8bd1b3bd36de83e5f87b1bceb4
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 00:02:36 +0700

    add chatbot page

---
+++ resources/views/chatbot/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.main')
+
+@section('style')
+    <link rel="stylesheet" href="./assets/compiled/css/ui-widgets-chatbox.css" />
+@endsection
+
+@section('content')
+    <div id="main-content">
+        <div class="page-heading">
+            <div class="page-title">
+                <div class="row">
+                    <div class="col-12 col-md-6 mb-4 order-md-1 order-last">
+                        <h3>{{ $title }}</h3>
+                    </div>
+                    <div class="col-12 col-md-6 order-md-2 order-first">
+                        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="/dashboard">Dashboard</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    {{ $title }}
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <section class="section">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card ">
+                            <div class="card-header">
+                                <div class="media d-flex align-items-center">
+                                    <div class="avatar me-3">
+                                        <img src="./assets/compiled/jpg/1.jpg" alt="" srcset="" />
+                                        <span class="avatar-status bg-success"></span>
+                                    </div>
+                                    <div class="name flex-grow-1">
+                                        <h6 class="mb-0">ChatGPT</h6>
+                                        <span class="text-xs">Online</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body pt-4 bg-grey">
+                                <div class="chat-content" id="chat-content">
+                                    <template id="chat-right">
+                                        <div class="chat">
+                                            <div class="chat-body">
+                                                <div class="chat-message">
+                                                    <p id="chat-message"></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </template>
+
+                                    <template id="chat-left">
+                                        <div class="chat chat-left">
+                                            <div class="chat-body">
+                                                <div class="chat-message">
+                                                    <p id="chat-message"></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </template>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <form id="chatbot">
+                                    @csrf
+                                    <div class="message-form d-flex flex-direction-column align-items-center">
+                                        <div class="d-flex flex-grow-1 ms-0 me-0">
+                                            <textarea name="message" class="form-control" placeholder="Type your message.." required rows="3"></textarea>
+                                        </div>
+                                        <button type="submit" id="submit" class="black ms-3 border-0 bg-0" data-feather="send"></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+
+@endsection
