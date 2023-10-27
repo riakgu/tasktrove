6a2b5f5039eb2459b1749ad74443e780d083fa06
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 00:49:54 +0700

    style: increase chatbot card height

--- resources/views/chatbot/index.blade.php
+++ resources/views/chatbot/index.blade.php
@@ -29,7 +29,7 @@
             <section class="section">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="card ">
+                        <div class="card " style="height: 500px;">
                             <div class="card-header">
                                 <div class="media d-flex align-items-center">
                                     <div class="avatar me-3">
