915220bd68aca77f334ab55ee355fa2cf164955f
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 15:18:38 +0700

    add middleware to each routes

--- routes/web.php
+++ routes/web.php
@@ -18,27 +18,26 @@ Route::get('/', function () {
 });
 
 Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
-    Route::get('/login', 'login')->name('login');
-    Route::post('/login', 'doLogin');
-    Route::get('/register', 'register')->name('register');
-    Route::post('/register', 'doRegister');
-    Route::post('/logout', 'doLogout');
+    Route::get('/login', 'login')->name('login')->middleware('guest');
+    Route::post('/login', 'doLogin')->middleware('guest');
+    Route::get('/register', 'register')->name('register')->middleware('guest');
+    Route::post('/register', 'doRegister')->middleware('guest');
+    Route::post('/logout', 'doLogout')->middleware('auth');
 });
 
 Route::controller(\App\Http\Controllers\DashboardController::class)->group(function () {
-    Route::get('/dashboard', 'index');
+    Route::get('/dashboard', 'index')->middleware('auth');
+});
+
+Route::resource('tasks', \App\Http\Controllers\TaskController::class)->middleware('auth');
+
+Route::controller(\App\Http\Controllers\ChatbotController::class)->group(function () {
+    Route::get('/chatbot', 'index')->middleware('auth');
+    Route::post('/chatbot', 'chatbot')->middleware('auth');
 });
 
 Route::controller(\App\Http\Controllers\SettingsController::class)->group(function () {
-    Route::get('/settings', 'index');
-    Route::put('/settings/profile', 'profile');
-    Route::put('/settings/password', 'password');
+    Route::get('/settings', 'index')->middleware('auth');
+    Route::put('/settings/profile', 'profile')->middleware('auth');
+    Route::put('/settings/password', 'password')->middleware('auth');
 });
-
-Route::controller(\App\Http\Controllers\ChatbotController::class)->group(function () {
-    Route::get('/chatbot', 'index');
-    Route::post('/chatbot', 'chatbot');
-});
-
-Route::resource('tasks', \App\Http\Controllers\TaskController::class);
-
