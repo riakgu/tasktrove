3917460911558e6b736a402ea245d8b786e08f21
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 13:01:04 +0700

    style: change footer app name

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2023 &copy; TaskTrove</p>
+            <p>2023 &copy; {{ env('APP_NAME') }}</p>
         </div>
     </div>
 </footer>
