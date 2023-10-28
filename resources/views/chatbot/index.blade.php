624e56b30b584b4a4158b2b000291d8d58de740a
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 13:55:20 +0700

    style: delete breadcrumb

--- resources/views/chatbot/index.blade.php
+++ resources/views/chatbot/index.blade.php
@@ -12,18 +12,6 @@
                     <div class="col-12 col-md-6 mb-4 order-md-1 order-last">
                         <h3>{{ $title }}</h3>
                     </div>
-                    <div class="col-12 col-md-6 order-md-2 order-first">
-                        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item">
-                                    <a href="/dashboard">Dashboard</a>
-                                </li>
-                                <li class="breadcrumb-item active" aria-current="page">
-                                    {{ $title }}
-                                </li>
-                            </ol>
-                        </nav>
-                    </div>
                 </div>
             </div>
             <section class="section">
