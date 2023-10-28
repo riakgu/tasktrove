b5b12aa5c62f1d15bb992e4a9d446093113a8d52
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-28 11:57:26 +0700

    style: fix sidebar link

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,41 +9,30 @@
         </div>
         <div class="sidebar-menu">
             <ul class="menu">
-                <!-- <li class="sidebar-title">Menu</li> -->
 
                 <li class="sidebar-item {{ request()->is('dashboard') ? 'active' : '' }}">
-                    <a href="dashboard.html" class="sidebar-link">
+                    <a href="/dashboard" class="sidebar-link">
                         <i class="bi bi-house-door-fill"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
 
-                <li class="sidebar-item {{ request()->is('tasks*') ? 'active' : '' }} has-sub">
-                    <a href="#" class="sidebar-link">
+                <li class="sidebar-item {{ request()->is('tasks*') ? 'active' : '' }}">
+                    <a href="/tasks" class="sidebar-link">
                         <i class="bi bi-journal-bookmark-fill"></i>
                         <span>Tasks</span>
                     </a>
-
-                    <ul class="submenu">
-                        <li class="submenu-item {{ request()->is('tasks/add') ? 'active' : '' }}">
-                            <a href="add-task.html" class="submenu-link">Add Task</a>
-                        </li>
-
-                        <li class="submenu-item {{ request()->is('tasks/list') ? 'active' : '' }}">
-                            <a href="task-list.html" class="submenu-link">Task List</a>
-                        </li>
-                    </ul>
                 </li>
 
                 <li class="sidebar-item {{ request()->is('chatbot') ? 'active' : '' }}">
-                    <a href="chatbot.html" class="sidebar-link">
+                    <a href="/chatbot" class="sidebar-link">
                         <i class="bi bi-chat-dots-fill"></i>
                         <span>Chatbot</span>
                     </a>
                 </li>
 
                 <li class="sidebar-item {{ request()->is('settings') ? 'active' : '' }}">
-                    <a href="settings.html" class="sidebar-link">
+                    <a href="/settings" class="sidebar-link">
                         <i class="bi bi-gear-fill"></i>
                         <span>Settings</span>
                     </a>
