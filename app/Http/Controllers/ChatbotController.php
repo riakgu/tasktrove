ae8f2a2655d4ae38bbce50e63679e5cb4f8c2775
riakgu <rizkyakbargusnaen@gmail.com>
2023-10-31 08:33:55 +0700

    add try catch in chatbot function

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -18,25 +18,31 @@ class ChatbotController extends Controller
     public function chatbot(Request $request) {
         $message = $request->input('message');
 
-        $data = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer '.env('OPENAI_API_KEY')
-        ])->post('https://api.openai.com/v1/chat/completions', [
-            'model'=> 'gpt-3.5-turbo',
-            'messages' => [
-                [
-                    'role' => 'user',
-                    'content' => $message
-                ]
-            ],
-            'temperature' => 0.5,
-            'max_tokens' => 200,
-            'top_p' => 1.0,
-            'frequency_penalty' => 0.52,
-            'presence_penalty' => 0.5,
-            'stop' => ['11.']
-        ])->json();
+        try {
+            $data = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . env('OPENAI_API_KEY')
+            ])->post('https://api.openai.com/v1/chat/completions', [
+                'model' => 'gpt-3.5-turbo',
+                'messages' => [
+                    [
+                        'role' => 'user',
+                        'content' => $message
+                    ]
+                ],
+                'temperature' => 0.5,
+                'max_tokens' => 200,
+                'top_p' => 1.0,
+                'frequency_penalty' => 0.52,
+                'presence_penalty' => 0.5,
+                'stop' => ['11.']
+            ])->json();
 
-        return response()->json($data['choices'][0]['message']['content']);
+            return response()->json($data['choices'][0]['message']['content']);
+        } catch (\Exception $e) {
+            return response()->json('An error occurred while processing your request');
+            // return response()->json($data['error']['message']);
+        }
     }
+
 }
